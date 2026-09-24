8900ac87a376700bc2aff944805ffb71f3c420c5
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-09-24 13:30:42 +0100

    new commit

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -87,7 +87,7 @@ export default function Login() {
             console.log(error)
             Swal.fire({
                 title: "Error",
-                icon: "error",
+                icon: "info",
                 text: error.message
             })
         }
