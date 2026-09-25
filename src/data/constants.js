bce7b44de4ef8660cd2752f3684f987a2fe730de
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-09-25 10:39:39 +0100

    new commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,2 +1,2 @@
-// export const backendUrl = "http://localhost:8000";
-export const backendUrl = "https://store-pilot-backend-c3d0.onrender.com";
+export const backendUrl = "http://localhost:8000";
+// export const backendUrl = "https://store-pilot-backend-c3d0.onrender.com";
